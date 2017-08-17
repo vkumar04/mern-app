58632314cc89cde8a1faebab812317a099367594
Vick Kumar <vick.kumar44@gmail.com>
2017-08-17 11:38:32 -0400

    added oauth keys

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+// clientID 1026024051176-ig1flkc05tab9jq1st7l8i0l706sf1nv.apps.googleusercontent.com
+// clientSecret f55FVh686iiIFn4C_8plI4_g
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
   res.send({hi: 'there'});
