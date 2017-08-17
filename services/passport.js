59ee2d93a0f2e23ea677be93249fe7ca067c8103
Vick Kumar <vick.kumar44@gmail.com>
2017-08-17 16:42:42 -0400

    added serializer, deserializer

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
 passport.use(
   new GoogleStrategy({
   clientID: keys.googleClientID,
@@ -12,8 +23,15 @@ passport.use(
   callbackURL: '/auth/google/callback'
   }, 
   (accessToken, refreshToken, profile, done) => {
-    new User({
-      googleId: profile.id
-    }).save();
+    User.findOne({googleId: profile.id})
+      .then((existingUser) => {
+        if(existingUser){
+          done(null, existingUser);
+        }
+        else {
+          new User({googleId: profile.id}).save()
+            .then(user => done(null, user));
+        }
+      })
   })
 );
