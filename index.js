e5e049112dd3d65a427c0ce222393eff45a89beb
Vick Kumar <vick.kumar44@gmail.com>
2017-08-18 07:35:06 -0400

    created current user route

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = 5000
